21c84b1c0449e477219079a415d7741d0da9e8a4
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-01-30 10:13:47 -0500

    random styles

--- src/App.svelte
+++ src/App.svelte
@@ -3,32 +3,43 @@
   import Generator from './lib/Generator.svelte'
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div class="w-96 h-52 bg-fuchsia-500/10 blur-3xl absolute bottom-[50%] right-[50%]"></div>
+<div class="w-96 h-52 bg-purple-500/10 blur-3xl absolute top-[50%] left-[50%]"></div>
+<div class="w-52 h-80 bg-pink-500/10 blur-3xl absolute top-[35%] right-[45%]"></div>
+<main class="flex flex-col items-center min-h-screen justify-center">
 
-  <div class="card">
-    <Generator />
+  <div>
+    <div class="relative space-y-5 overflow-hidden rounded-2xl bg-white/5 p-4 shadow-xl shadow-black/5">
+      <div class="h-36 rounded-lg bg-white/5 flex items-center">
+        <div class="flex items-center">
+          <div>
+            <span class="left-bracket">&#123;</span>
+          </div>
+          <div class="flex gap-3">
+            <div class="eye"></div>
+            <div class="eye invisible"></div>
+            <div class="eye"></div>
+          </div>
+          <div>
+            <span class="left-bracket">&#125;</span>
+          </div>
+        </div>
+      </div>
+      <Generator />
+    </div>
   </div>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+<style lang="postcss">
+  .left-bracket {
+    @apply font-semibold text-transparent text-9xl bg-clip-text bg-gradient-to-b from-rose-400 via-fuchsia-500 to-indigo-500;
+    @apply !leading-[.3];
+    text-shadow: #fb7185 0 0 10px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .eye {
+    @apply bg-white;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
   }
 </style>
--- src/lib/Generator.svelte
+++ src/lib/Generator.svelte
@@ -10,8 +10,10 @@
   }
 </script>
 
-<textarea bind:value={function_goal} class="border" name="function goal" rows="5" placeholder="Write your function goal"></textarea>
-
-<button on:click={generate}>
-  Generar nombre
-</button>
+<div class="flex flex-col">
+  <textarea bind:value={function_goal} class="border" name="function goal" rows="5" placeholder="Write your function goal"></textarea>
+  <button on:click={generate} class="relative overflow-hidden rounded-lg bg-black px-20 py-6 ring-red-500/50 ring-offset-black will-change-transform focus:outline-none focus:ring-1 focus:ring-offset-2">
+    <span class="absolute inset-px z-10 grid place-items-center rounded-lg bg-black bg-gradient-to-t from-neutral-800 text-neutral-400">Generar nombre</span>
+    <span aria-hidden class="absolute inset-0 z-0 scale-x-[2.0] blur before:absolute before:inset-0 before:top-1/2 before:aspect-square before:animate-disco before:bg-[conic-gradient(var(--tw-gradient-stops))] before:from-purple-700 before:via-red-500 before:to-amber-400" />
+  </button>
+</div>
